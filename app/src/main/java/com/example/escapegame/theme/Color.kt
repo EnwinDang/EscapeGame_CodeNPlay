6d7cf094c5b49a7c42b08c31ae3245498387c6ac
EnwinDang <n9uyend4ng@gmail.com>
2026-03-10 22:27:05 +0100

    test

--- app/src/main/java/com/example/escapegame/theme/Color.kt
+++ app/src/main/java/com/example/escapegame/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.escapegame.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Mission Control palette
+val DarkBackground     = Color(0xFF0A0E1A)
+val DarkSurface        = Color(0xFF111827)
+val DarkSurfaceVariant = Color(0xFF1A2540)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val CyanPrimary        = Color(0xFF00D4FF)
+val CyanDark           = Color(0xFF003D4F)
+val CyanLight          = Color(0xFFB3ECFF)
+
+val NeonGreen          = Color(0xFF4ADE80)
+val NeonGreenDark      = Color(0xFF0D2400)
+val NeonGreenLight     = Color(0xFFBBF7D0)
+
+val OnDark             = Color(0xFFDCF0F8)
+val OnDarkMuted        = Color(0xFF8BA5BE)
+
+val AlertRed           = Color(0xFFFF5555)
+val AlertRedDark       = Color(0xFF3D0808)
+val AlertRedLight      = Color(0xFFFFAAAA)
+
+val PanelBorder        = Color(0xFF1E3A5F)
