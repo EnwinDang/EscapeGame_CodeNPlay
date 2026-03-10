431c0f891645ee35e57b0d632407454a9abdf59d
EnwinDang <n9uyend4ng@gmail.com>
2026-03-10 21:16:20 +0100

    linking fix

--- app/src/main/java/com/example/escapegame/MainActivity.kt
+++ app/src/main/java/com/example/escapegame/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.escapegame
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.escapegame.navigation.NavGraph
 import com.example.escapegame.theme.EscapeGameTheme
 import com.example.escapegame.viewmodel.GameViewModel
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
